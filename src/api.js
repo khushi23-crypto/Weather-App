f50753cbc6e8bff17af1b7c790e8cdcf60329d58
Kajal Saini <sizukabts91@gmail.com>
2025-09-05 15:46:03 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3abf318eb1msh5955ce82590ca16p1d8eafjsn4e136ca0c44a',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY ="21bf5bd44e3bbf9e402f61373a34351d";
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-left">
+        <h2 className="footer-logo">MyWebsite</h2>
+        <p>Bringing you closer to the web.</p>
+      </div>
+
+      <div className="footer-center">
+        <a href="/">Home</a>
+        <a href="/services">Services</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+
+      <div className="footer-right">
+        <p>Follow us</p>
+        <div className="social-icons">
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">📘</a>
+          <a href="https://twitter.com" target="_blank" rel="noreferrer">🐦</a>
+          <a href="https://instagram.com" target="_blank" rel="noreferrer">📸</a>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        © 2025 MyWebsite. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
